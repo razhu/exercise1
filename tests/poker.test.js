c2a68b1137463d8fb6b23613f76907fdf454816c
ramiro <ramihertz@gmail.com>
2022-10-14 09:15:29 -0400

    update readme

--- tests/poker.test.js
+++ tests/poker.test.js
@@ -1,14 +1,14 @@
 const determineRanks = require('../poker')
  
-test('It\'s a 2As', () => {
+test('[Ah As 10c 7d 6s] is a 2As', () => {
     expect(determineRanks('Ah As 10c 7d 6s')).toBe('It\'s a Pair of Aces!');
 });
-test('It\'s a 2 Pair', () => {
+test('[Kh Kc 3s 3h 2d] is a 2 Pair', () => {
     expect(determineRanks('Kh Kc 3s 3h 2d')).toBe('It\'s a 2 Pair!');
 });
-test('It\'s a Flush', () => {
+test('[Kh Qh 6h 2h 9h] is a Flush', () => {
     expect(determineRanks('Kh Qh 6h 2h 9h')).toBe('It\'s a Flush!');
 });
-test('Invalid hand', () => {
+test('[3d Qh 6h 2h 9h] is invalid hand', () => {
     expect(determineRanks('3d Qh 6h 2h 9h')).toBe('Hand is invalid!');
 });
